e1987170bf7c79bf81e6616f6e422fe09da2862e
ericb <eric_bastarache@hotmail.com>
2017-03-04 01:49:43 -0500

    Preliminary footer markup in place

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="text-center">Copyright &copy; 2017</h1>
+      </div>
+    </div>
+  </div>
+</footer>
