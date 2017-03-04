81eb5257f7ddb2fa2e96244a1c548aecd9bc2b34
ericb <eric_bastarache@hotmail.com>
2017-03-04 01:49:57 -0500

    Updated navbar brand

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,7 +7,9 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#"></a>
+      <a class="navbar-brand" href="#">
+        {{ config('app.name', 'Laravel') }}
+      </a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
